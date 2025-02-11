a6298032128b5c26319c6dc60e55eb8d60018712
fengxiao1234k <2330739176@qq.com>
2025-02-12 03:04:14 +0800

    Update and rename Localisation (2).sql to OutriderAmber/Scripts/Localisation.sql

---
+++ OutriderAmber/Scripts/Localisation.sql
@@ -0,0 +1,24 @@
+-- Localisation
+-- Author: stanr
+-- DateCreated: 8/5/2024 7:55:17 PM
+--------------------------------------------------------------
+
+INSERT INTO LocalizedText
+	(Language,	Tag,								Text													)
+VALUES
+	('en_US',	'LOC_SR_GOODY_CULTURE_NAME',		'获得一个政治鼓舞 [ICON_CivicBoosted]'		),
+	('en_US',	'LOC_SR_GOODY_SCIENCE_NAME',		'获得一个科技尤里卡 [ICON_TechBoosted]'	),
+	('en_US',	'LOC_SR_GOODY_GOLD_NAME',			'获得随机数量金币 [ICON_Gold]'					),
+	('en_US',	'LOC_SR_GOODY_FAITH_NAME',			'获得随机数量信仰值 [ICON_Faith]'					),
+	('en_US',	'LOC_SR_GOODY_RELIC_NAME',			'获得遗物 [ICON_GreatWork_Relic]'						),
+	('en_US',	'LOC_SR_GOODY_SCOUT_NAME',			'获得 {LOC_UNIT_SCOUT_NAME}'								),
+	('en_US',	'LOC_SR_GOODY_HEAL_NAME',			'治疗当前单位'										),
+	('en_US',	'LOC_SR_GOODY_POP_NAME',			'+1 人口 [ICON_Citizen]'							),
+	('en_US',	'LOC_SR_GOODY_BUILDER_NAME',		'获得 {LOC_UNIT_BUILDER_NAME}'								),
+	('en_US',	'LOC_SR_GOODY_TRADER_NAME',			'获得 {LOC_UNIT_TRADER_NAME}'								),
+	('en_US',	'LOC_SR_GOODY_GOVERNOR_NAME',		'+1 总督 [ICON_Governor]'						),
+	('en_US',	'LOC_SR_GOODY_ENVOY_NAME',			'+1 使者 [ICON_Envoy]'									),
+	('en_US',	'LOC_SR_GOODY_FAVOR_NAME',			'+20 外交支持 [ICON_Favor]'						),
+	('en_US',	'LOC_SR_GOODY_RESOURCES_NAME',		'+20 随机战略资源'						),
+	('en_US',	'LOC_SR_GOODY_EXPERIENCE_NAME',		'+20 经验给予当前单位'									),
+	('en_US',	'LOC_SR_GOODY_CHOICE_NAME',			'请选择您所想要的村民礼物'									);

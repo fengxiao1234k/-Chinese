4a4f1888c9b8d1281ab421b140b8bb644f9863a3
fengxiao1234k <2330739176@qq.com>
2025-02-11 22:42:20 +0800

    Add files via upload

---
+++ Localisation (3).sql
@@ -0,0 +1,96 @@
+-- Localisation
+-- Author: stanr
+-- DateCreated: 8/1/2024 5:13:07 PM
+--------------------------------------------------------------
+
+
+INSERT INTO LocalizedText
+			(Language, Tag, Text)
+VALUES
+	('en_US',	'LOC_CIVILIZATION_SR_MONDSTADT_NAME',			'蒙德'						),
+	('en_US',	'LOC_CIVILIZATION_SR_MONDSTADT_DESCRIPTION',	'蒙德, 自由之都'	),
+	('en_US',	'LOC_CIVILIZATION_SR_MONDSTADT_ADJECTIVE',  	'蒙德'						),
+
+	('en_US',	'LOC_TRAIT_CIVILIZATION_SR_WIND_GLIDERS_AND_WINE_NAME',				'风之翼 与 葡萄酒'				),
+	('en_US',	'LOC_TRAIT_CIVILIZATION_SR_WIND_GLIDERS_AND_WINE_DESCRIPTION',	
+	'非侦察军事单位和支援陆地单位无视来自丘陵的移动惩罚。[NEWLINE]种植园额外 +2 文化值 [ICON_Culture] 以及 +1 宜居度 [ICON_Amenities]。'	),
+
+	('en_US',	'LOC_ABILITY_SR_WIND_GLIDER_NAME',			'风之翼'									),
+	('en_US',	'LOC_ABILITY_SR_WIND_GLIDER_DESCRIPTION',	'无视来自丘陵的移动惩罚'		),
+
+
+	('en_US',	'LOC_BUILDING_SR_FAVONIUS_CATHEDRAL_NAME',				'西风大教堂'								),
+	('en_US',	'LOC_BUILDING_SR_FAVONIUS_CATHEDRAL_DESCRIPTION',
+	'西风大教堂，是风神信徒们向巴巴托斯祈祷的宏伟圣堂。[NEWLINE]取代寺庙，解锁后可用信仰值购买使徒和审判官 [ICON_Faith]。[NEWLINE]建成后获得一个遗物 [ICON_GreatWork_Relic] 。 遗物所在城市+50%每回合伟人点数 [ICON_GreatPerson] 。'	),
+
+
+	('en_US',	'LOC_UNIT_SR_OUTRIDER_NAME',	'侦察骑士'	),
+	('en_US',	'LOC_UNIT_SR_OUTRIDER_DESCRIPTION',	 
+	'侦察骑士是西风骑士团的兵种之一。 取代侦察兵，拥有更强的近战攻击力 [ICON_Strength]，拥有更远的视野，拥有自选部落村庄奖励的能力。'	),
+
+	('en_US',	'LOC_ABILITY_SR_OUTRIDER',  '+8 [ICON_Strength] 攻击力。 +1 视野。 可以自选部落村庄的奖励。'	),
+
+
+	('en_US',	'LOC_NAMED_MOUNTAIN_DRAGONSPINE',			'龙脊雪山'			),
+	('en_US',	'LOC_NAMED_MOUNTAIN_BRIGHTCROWN_MOUNTAIN',	'明冠山地'	),
+	('en_US',	'LOC_NAMED_MOUNTAIN_STORMBEARER_MOUNTAIN',	'望风山地'	),
+
+
+	('en_US',	'LOC_NAMED_LAKE_CIDER_LAKE',				'果酒湖'		),
+	('en_US',	'LOC_NAMED_LAKE_STARFELL_LAKE',				'星落湖'		),
+
+	('en_US',	'LOC_NAMED_SEA_MUSK_REEF',					'马斯克礁'		),
+	('en_US',	'LOC_NAMED_SEA_FALCON_COAST',				'鹰翔海滩'		),
+
+
+	('en_US',	'LOC_CITY_NAME_MONDSTADT_CITY',  '蒙德城'),
+	('en_US',	'LOC_CITY_NAME_DAWN_WINERY',  '晨曦酒庄'),
+	('en_US',	'LOC_CITY_NAME_SPRINGVALE',  '清泉镇'),
+	('en_US',	'LOC_CITY_NAME_WOLVENDOM',  '奔狼领'),
+	('en_US',	'LOC_CITY_NAME_THOUSAND_WINDS_TEMPLE',  '千风神殿'),
+	('en_US',	'LOC_CITY_NAME_GALESONG_HILL',  '风啸山坡'),
+	('en_US',	'LOC_CITY_NAME_STARFELL_VALLEY',  '坠星山谷'),
+	('en_US',	'LOC_CITY_NAME_CAPE_OATH',  '誓言岬'),
+	('en_US',	'LOC_CITY_NAME_DADAUPA_GORGE',  '达达乌帕谷'),
+	('en_US',	'LOC_CITY_NAME_BRIGHTCROWN_CANYON',  '明冠峡'),
+	('en_US',	'LOC_CITY_NAME_STORMTERRORS_LAIR',  '风龙废墟'),
+	('en_US',	'LOC_CITY_NAME_WYRMREST_VALLEY',  '眠龙谷'),
+	('en_US',	'LOC_CITY_NAME_STARGLOW_CAVERN',  '星荧洞窟'),
+	('en_US',	'LOC_CITY_NAME_ENTOMBED_CITY',  '雪葬之都'),
+	('en_US',	'LOC_CITY_NAME_WHISPERING_WOODS',  '低语森林'),
+	('en_US',	'LOC_CITY_NAME_WINDWAIL_HIGHLAND',  '苍风高地'),
+	
+
+	('en_US',	'LOC_CITIZEN_SR_ALBEDO',	'阿贝多'	),
+	('en_US',	'LOC_CITIZEN_SR_BENNETT',	'班尼特'	),
+	('en_US',	'LOC_CITIZEN_SR_DILUC',		'迪卢克'		),
+	('en_US',	'LOC_CITIZEN_SR_KAEYA',		'凯亚'		),
+	('en_US',	'LOC_CITIZEN_SR_MIKA',		'米卡'		),
+	('en_US',	'LOC_CITIZEN_SR_RAZOR',		'雷泽'		),
+	('en_US',	'LOC_CITIZEN_SR_VENTI',		'温迪'		),
+	('en_US',	'LOC_CITIZEN_SR_AMBER',		'安柏'		),
+	('en_US',	'LOC_CITIZEN_SR_BARBARA',	'芭芭拉'	),
+	('en_US',	'LOC_CITIZEN_SR_DIONA',		'迪奥娜'		),
+	('en_US',	'LOC_CITIZEN_SR_EULA',		'优菈'		),
+	('en_US',	'LOC_CITIZEN_SR_FISCHL',	'菲谢尔'	),
+	('en_US',	'LOC_CITIZEN_SR_JEAN',		'琴'		),
+	('en_US',	'LOC_CITIZEN_SR_KLEE',		'可莉'		),
+	('en_US',	'LOC_CITIZEN_SR_LISA',		'丽莎'		),
+	('en_US',	'LOC_CITIZEN_SR_MONA',		'莫娜'		),
+	('en_US',	'LOC_CITIZEN_SR_NOELLE',	'诺艾尔'	),
+	('en_US',	'LOC_CITIZEN_SR_ROSARIA',	'罗莎莉亚'	),
+	('en_US',	'LOC_CITIZEN_SR_SUCROSE',	'砂糖'	),
+	
+
+	('en_US',	'LOC_CIVINFO_SR_MONDSTADT_LOCATION',		'提瓦特'			),
+	('en_US',	'LOC_CIVINFO_SR_MONDSTADT_SIZE',			'3.87 平方公里'			),
+	('en_US',	'LOC_CIVINFO_SR_MONDSTADT_POPULATION',		'约 1000'			),
+	('en_US',	'LOC_CIVINFO_SR_MONDSTADT_CAPITAL',			'蒙德城'	),
+
+
+	('en_US',	'LOC_PEDIA_CIVILIZATIONS_PAGE_CIVILIZATION_SR_MONDSTADT_CHAPTER_HISTORY_PARA_1',		
+	'一座位于提瓦特东北部的自由之都。 从群山和开阔的平原中，无忧无虑的微风吹拂而来的蒲公英香 —— 这是风神巴巴托斯的礼物 —— 蒙德城被果酒湖包围，坐落在湖中央岛屿。'),	
+	
+
+	('en_US',	'LOC_PEDIA_UNITS_PAGE_UNIT_SR_MONDSTADT_UU_CHAPTER_HISTORY_PARA_1',  	
+	'侦察骑士，是安柏的祖父一手建立并率领的，西风骑士团骑士兵种分支之一。 侦察骑士接受了安柏祖父的训练，学会了他的射箭招式和使用风之翼的技巧，并以其技能而闻名。'	);

b7dd6cef5c3969b77689dd3993aac98fefa8855c
fengxiao1234k <2330739176@qq.com>
2025-02-11 15:53:47 +0800

    Add files via upload

---
+++ Localisation.sql
@@ -0,0 +1,97 @@
+-- Localisation
+-- Author: stanr
+-- DateCreated: 8/1/2024 5:13:07 PM
+--------------------------------------------------------------
+
+
+INSERT INTO LocalizedText
+			(Language, Tag, Text)
+VALUES
+	('en_US',	'LOC_CIVILIZATION_SR_MONDSTADT_NAME',			'Mondstadt'						),
+	('en_US',	'LOC_CIVILIZATION_SR_MONDSTADT_DESCRIPTION',	'Mondstadt, City of Freedom'	),
+	('en_US',	'LOC_CIVILIZATION_SR_MONDSTADT_ADJECTIVE',  	'Mondstadt'						),
+
+	('en_US',	'LOC_TRAIT_CIVILIZATION_SR_WIND_GLIDERS_AND_WINE_NAME',				'Wind Gliders and Wine'				),
+	('en_US',	'LOC_TRAIT_CIVILIZATION_SR_WIND_GLIDERS_AND_WINE_DESCRIPTION',	
+	'Non-Recon Military and Support Land units ignore the movement penalty from Hills.[NEWLINE]Plantations yield an additional +2 Culture [ICON_Culture] and +1 Amenity [ICON_Amenities].'	),
+
+	('en_US',	'LOC_ABILITY_SR_WIND_GLIDER_NAME',			'Wind Glider'									),
+	('en_US',	'LOC_ABILITY_SR_WIND_GLIDER_DESCRIPTION',	'Unit ignores movement penalty from hills'		),
+
+
+	('en_US',	'LOC_BUILDING_SR_FAVONIUS_CATHEDRAL_NAME',				'Favonius Cathedral'								),
+	('en_US',	'LOC_BUILDING_SR_FAVONIUS_CATHEDRAL_DESCRIPTION',
+	'The Church of Favonius'' place of worship to Barbatos.[NEWLINE]Replaces the Temple. Allows the purchasing of Apostles and Inquisitors with Faith [ICON_Faith].[NEWLINE]Grants a Relic [ICON_GreatWork_Relic] upon completion. +50% [ICON_GreatPerson] Great People points generated per turn in the city.'	),
+
+
+	('en_US',	'LOC_UNIT_SR_OUTRIDER_NAME',	'Outrider'	),
+	('en_US',	'LOC_UNIT_SR_OUTRIDER_DESCRIPTION',	 
+	'A unit of the Knights of Favonius Outrider division. Replaces the scout with a stronger combat strength [ICON_Strength], vision and the ability to choose Tribal Village boons.'	),
+
+	('en_US',	'LOC_ABILITY_SR_OUTRIDER',  '+8 [ICON_Strength] Combat Strength. +1 Sight. Can choose bonuses from Tribal Villages.'	),
+
+
+	('en_US',	'LOC_NAMED_MOUNTAIN_DRAGONSPINE',			'Dragonspine'			),
+	('en_US',	'LOC_NAMED_MOUNTAIN_BRIGHTCROWN_MOUNTAIN',	'Brightcrown Mountain'	),
+	('en_US',	'LOC_NAMED_MOUNTAIN_STORMBEARER_MOUNTAIN',	'Stormbearer Mountain'	),
+
+
+	('en_US',	'LOC_NAMED_LAKE_CIDER_LAKE',				'Cider Lake'		),
+	('en_US',	'LOC_NAMED_LAKE_STARFELL_LAKE',				'Starfell Lake'		),
+
+	('en_US',	'LOC_NAMED_SEA_MUSK_REEF',					'Musk Reef'		),
+	('en_US',	'LOC_NAMED_SEA_FALCON_COAST',				'Falcon Coast'		),
+
+
+	('en_US',	'LOC_CITY_NAME_MONDSTADT_CITY',  'Mondstadt City'),
+	('en_US',	'LOC_CITY_NAME_DAWN_WINERY',  'Dawn Winery'),
+	('en_US',	'LOC_CITY_NAME_SPRINGVALE',  'Springvale'),
+	('en_US',	'LOC_CITY_NAME_WOLVENDOM',  'Wolvendom'),
+	('en_US',	'LOC_CITY_NAME_THOUSAND_WINDS_TEMPLE',  'Thousand Winds Temple'),
+	('en_US',	'LOC_CITY_NAME_GALESONG_HILL',  'Galesong Hill'),
+	('en_US',	'LOC_CITY_NAME_STARFELL_VALLEY',  'Starfell Valley'),
+	('en_US',	'LOC_CITY_NAME_CAPE_OATH',  'Cape Oath'),
+	('en_US',	'LOC_CITY_NAME_DADAUPA_GORGE',  'Dadaupa Gorge'),
+	('en_US',	'LOC_CITY_NAME_BRIGHTCROWN_CANYON',  'Brightcrown Canyon'),
+	('en_US',	'LOC_CITY_NAME_STORMTERRORS_LAIR',  'Stormterrors Lair'),
+	('en_US',	'LOC_CITY_NAME_WYRMREST_VALLEY',  'Wyrmrest Valley'),
+	('en_US',	'LOC_CITY_NAME_STARGLOW_CAVERN',  'Starglow Cavern'),
+	('en_US',	'LOC_CITY_NAME_ENTOMBED_CITY',  'Entombed City'),
+	('en_US',	'LOC_CITY_NAME_WHISPERING_WOODS',  'Whispering Woods'),
+	('en_US',	'LOC_CITY_NAME_WINDWAIL_HIGHLAND',  'Windwail Highland'),
+	
+
+	('en_US',	'LOC_CITIZEN_SR_ALBEDO',	'Albedo'	),
+	('en_US',	'LOC_CITIZEN_SR_BENNETT',	'Bennett'	),
+	('en_US',	'LOC_CITIZEN_SR_DILUC',		'Diluc'		),
+	('en_US',	'LOC_CITIZEN_SR_KAEYA',		'Kaeya'		),
+	('en_US',	'LOC_CITIZEN_SR_MIKA',		'Mika'		),
+	('en_US',	'LOC_CITIZEN_SR_RAZOR',		'Razor'		),
+	('en_US',	'LOC_CITIZEN_SR_VENTI',		'Venti'		),
+	('en_US',	'LOC_CITIZEN_SR_AMBER',		'Amber'		),
+	('en_US',	'LOC_CITIZEN_SR_BARBARA',	'Barbara'	),
+	('en_US',	'LOC_CITIZEN_SR_DIONA',		'Diona'		),
+	('en_US',	'LOC_CITIZEN_SR_EULA',		'Eula'		),
+	('en_US',	'LOC_CITIZEN_SR_FISCHL',	'Fischl'	),
+	('en_US',	'LOC_CITIZEN_SR_JEAN',		'Jean'		),
+	('en_US',	'LOC_CITIZEN_SR_KLEE',		'Klee'		),
+	('en_US',	'LOC_CITIZEN_SR_LISA',		'Lisa'		),
+	('en_US',	'LOC_CITIZEN_SR_MONA',		'Mona'		),
+	('en_US',	'LOC_CITIZEN_SR_NOELLE',	'Noelle'	),
+	('en_US',	'LOC_CITIZEN_SR_ROSARIA',	'Rosaria'	),
+	('en_US',	'LOC_CITIZEN_SR_SUCROSE',	'Sucrose'	),
+	
+
+	('en_US',	'LOC_CIVINFO_SR_MONDSTADT_LOCATION',		'Teyvat'			),
+	('en_US',	'LOC_CIVINFO_SR_MONDSTADT_SIZE',			'3.87 km²'			),
+	('en_US',	'LOC_CIVINFO_SR_MONDSTADT_POPULATION',		'c. 1000'			),
+	('en_US',	'LOC_CIVINFO_SR_MONDSTADT_CAPITAL',			'Mondstadt City'	),
+
+
+	('en_US',	'LOC_PEDIA_CIVILIZATIONS_PAGE_CIVILIZATION_SR_MONDSTADT_CHAPTER_HISTORY_PARA_1',		
+	'A city of freedom that lies in the northeast of Teyvat. From amongst mountains and wide-open plains, carefree breezes carry the scent of dandelions — a gift from the Anemo God, Barbatos — across Cider Lake to Mondstadt, which sits on an island in the middle of the lake.'),	
+	
+
+	('en_US',	'LOC_PEDIA_UNITS_PAGE_UNIT_SR_MONDSTADT_UU_CHAPTER_HISTORY_PARA_1',  	
+	'A subdivision of the Knights established and led by Amber''s grandfather. They were trained in his style of archery and use of wind gliders, and were renowned for their skill'	);
+
